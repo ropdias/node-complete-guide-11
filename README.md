1e34e5bad1e629347723fbf288e0fb09440ac566
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2022-08-28 15:52:01 -0300

    Initial version

---
+++ README.md
@@ -0,0 +1,27 @@
+# Node.js: Adding Authentication
+
+Examples from the **"Section 15: Adding Authentication"** of the course **"NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)"** by Academind (Maximilian Schwarzmüller).
+
+Section 15 covered:
+
+- [X] What is Authentication?
+- [X] How is Authentication Implemented?
+- [X] Our Updated Project Status
+- [ ] Implementing an Authentication Flow
+- [ ] Encrypting Passwords
+- [ ] Adding a Tiny Code Improvement
+- [ ] Adding the Signin Functionality
+- [ ] Working on Route Protection
+- [ ] Using Middleware to Protect Routes
+- [ ] Understanding CSRF Attacks
+- [ ] Using a CSRF Token
+- [ ] Adding CSRF Protection
+- [ ] Fixing the Order Button
+- [ ] Providing User Feedback
+- [ ] Optional: Styling Error Messages
+- [ ] Finishing the Flash Messages
+- [ ] Adding Additional Flash Messages
+
+
+
+More info: https://www.udemy.com/course/nodejs-the-complete-guide/
---
+++ controllers/auth.js
@@ -0,0 +1,65 @@
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  // const isLoggedIn =
+  //   req.get("Cookie").split(";")[0].trim().split("=")[1] === "true";
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: req.session.isLoggedIn,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: req.session.isLoggedIn,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  // Cookies:
+  // Max-Age=10 // to expire in 10 seconds
+  // Domain=... // to set a domain
+  // Secure  // to set that the cookie should only work if the page is served via https
+  // HttpOnly // to set that we can't access the cookie value through client side javascript (scripts running in the browser)
+  // The one above can be important security mechanism because it protects us against cross-site scripting attacks now
+  // because now your client side javascript where someone could have injected malicious code can't read your cookies values and that
+  // will be important later with authentication where a cookie will not store the sensitive information but and important part of
+  // authenticating the user. This can be an extra security layer because now the cookie will still be attached to every request
+  // that is sent to the server but you can't read the cookie value from inside the browser javascript code. (In the developer tool
+  // you can still read it)
+  // res.setHeader("Set-Cookie", "loggedIn=true; Max-Age=10");
+
+  // The session object is added by the session middleware with app.use(session())
+  // req.session.isLoggedIn = true;
+  User.findById("630b9e957f011c15a62b4864")
+    .then((user) => {
+      if (user) {
+        req.session.isLoggedIn = true;
+        req.session.user = user; // This will remain a full mongoose model ONLY for this request
+        req.session.save((err) => {
+          if (err) console.log(err);
+          res.redirect("/");
+        });
+      } else {
+        console.log("User not found");
+        req.session.isLoggedIn = false;
+        req.session.save((err) => {
+          if (err) console.log(err);
+          res.redirect("/");
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) console.log(err);
+    res.redirect("/");
+  });
+};
